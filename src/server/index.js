cd8b8b724b50a3b3da4711ab074f3e44d12f36c0
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-04-16 16:15:31 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,31 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const cors = require("cors")
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(`Your API key is ${process.env.API_KEY}`)
+
+var textapi = new textapi({
+    application_key: process.env.API_KEY
+});
 
 const app = express()
 
-/*
-let textapi = new meaning({
-    application_key: process.env.API_KEY
-});
-*/
-
 app.use(express.static('dist'))
+
+app.use(cors())
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
