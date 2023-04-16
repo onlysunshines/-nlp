d69ab37741d2bdc34ca02dbb0fcfa2063af84060
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2023-04-16 16:14:55 -0600

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,18 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
+console.log(handleSubmit);
 console.log(checkForName);
+//console.log(Client);
+//console.log(Client.handleSubmit)
 
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit
+}
